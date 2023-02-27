ebe8f67cc406d6bc31503109b4cd19326aacd13f
stefan <stefan.will@gmail.com>
2023-02-27 01:19:22 -0500

    force python provider

--- ephem/settings.py
+++ ephem/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = [
    STATIC_DIR,
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
