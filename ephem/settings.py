1c325041960ebf3bfc32e9ee94540d7fd92952ee
stefan <stefan.will@gmail.com>
2023-04-01 19:57:20 -0400

    use google cloud secrets

--- ephem/settings.py
+++ ephem/settings.py
@@ -1,15 +1,46 @@
 from pathlib import Path
 import os
+import io
 from urllib.parse import urlparse
 import dj_database_url
 import environ
+import google.auth
+from google.cloud import secretmanager
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(
     DEBUG=(bool, True),
     ADMIN_ENABLED=(bool, True))
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+env_file = os.path.join(BASE_DIR, ".env")
+
+# Attempt to load the Project ID into the environment, safely failing on error.
+try:
+    _, os.environ["GOOGLE_CLOUD_PROJECT"] = google.auth.default()
+except google.auth.exceptions.DefaultCredentialsError:
+    pass
+
+if os.path.isfile(env_file):
+    # Use a local secret file, if provided
+
+    env.read_env(env_file)
+# ...
+elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
+    # Pull secrets from Secret Manager
+    project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
+
+    client = secretmanager.SecretManagerServiceClient()
+    settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
+    name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
+    payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
+
+    env.read_env(io.StringIO(payload))
+else:
+    raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
+# [END cloudrun_django_secret_config]
+SECRET_KEY = env("SECRET_KEY")
+
 TEMPLATE_DIR = BASE_DIR / "templates"
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
@@ -21,9 +52,6 @@ STATICFILES_DIRS = [
 ]
 
 STATICFILES_STORAGE = 'spa.storage.SPAStaticFilesStorage'
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-mbv$8^_f&=d90va4x+7ifmcynia@t9d!g#0&2wuv6-f%o89ian')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
